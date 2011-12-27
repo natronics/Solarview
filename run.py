47c426502e0ac6b453af80e91ae9a544aaa5d15c
Nathan Bergey <nathan.bergey@gmail.com>
2011-12-26 21:24:03 -0800

    clean up

--- run.py
+++ run.py
@@ -11,13 +11,13 @@ now = datetime.datetime.utcnow()
 p = solarsystem.position(ephem.Sun(), now)
 png = drawing.DrawPNG(720,720, 1.8, center=(0,0))
 
-png.draw_solarsystem(now, orbit_stroke=1.2, planet_scaler=2, planet_min=1.4, planet_max=7)
+png.draw_solarsystem(now, orbit_stroke=1.4, planet_scaler=3, planet_min=1.5, planet_max=7)
 
 spacecraft = solarsystem.spacecraft()
 
 for craft in spacecraft.crafts:
   orbit_past = spacecraft.get_past_orbit(craft, now)
-  png.draw_line(orbit_past, 1, (0.6,0.6,0.6))  
+  png.draw_line(orbit_past, 1, (0.6,0.6,0.6), 0.5)  
   scraft = spacecraft.get_location(craft, now)
   png.draw_object(scraft, 1, (0.95,0.95,0.95), 1)
 
